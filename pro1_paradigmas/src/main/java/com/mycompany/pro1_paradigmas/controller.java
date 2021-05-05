67d2972641d81268d5bf316a41da67ee571b0f99
JosueSandino-27 <josue.271168@gmail.com>
2021-05-05 12:59:40 -0600

    asd

--- pro1_paradigmas/src/main/java/com/mycompany/pro1_paradigmas/controller.java
+++ pro1_paradigmas/src/main/java/com/mycompany/pro1_paradigmas/controller.java
@@ -5,14 +5,47 @@
  */
 package com.mycompany.pro1_paradigmas;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author Josue
  */
 public class controller {
     
+    
+    
     public static void algo(String a){
         
+    }
+    
+    
+    public static void cargarTxt(){
+        
+        Scanner entrada = null;
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.showOpenDialog(fileChooser);
+        try {
+            String ruta = fileChooser.getSelectedFile().getAbsolutePath();                                        
+            File f = new File(ruta);
+            entrada = new Scanner(f);
+            while (entrada.hasNext()) {
+                System.out.println(entrada.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (NullPointerException e) {
+            System.out.println("No se ha seleccionado ningún fichero");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (entrada != null) {
+                entrada.close();
+            }
+        }
     }
     
         
