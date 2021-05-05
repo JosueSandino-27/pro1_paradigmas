1f55dac9d826b2aa6074da49ebda2a0f22eacead
JosueSandino-27 <49879640+JosueSandino-27@users.noreply.github.com>
2021-05-04 22:32:25 -0600

    Add files via upload

---
+++ pro1_paradigmas/src/main/java/com/mycompany/pro1_paradigmas/controller.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pro1_paradigmas;
+
+/**
+ *
+ * @author Josue
+ */
+public class controller {
+    
+    public static void algo(String a){
+        
+    }
+    
+        
+}
